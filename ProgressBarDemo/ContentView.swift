f03338b9d55891c63104f448db0fdf9bd2d343d5
Livsy90 <artmir@live.ru>
2024-09-05 12:37:27 +0400

    Initial Commit

--- ProgressBarDemo/ContentView.swift
+++ ProgressBarDemo/ContentView.swift
@@ -8,15 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var progress: CGFloat = .zero
+    @State private var progressBarValue: CGFloat = .zero
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(progress, format: .percent.precision(.fractionLength(.zero)))
+                .frame(maxWidth: .infinity, minHeight: 80)
+                .background {
+                    progressBar
+                }
+            
+            Slider(value: $progress, in: 0...1)
+                .padding()
         }
         .padding()
     }
+    
+    var progressBar: some View {
+        GeometryReader { geometry in
+            ZStack {
+                RoundedRectangle(cornerRadius: 16, style: .continuous)
+                    .fill(.green)
+                
+                RoundedRectangle(cornerRadius: 16, style: .continuous)
+                    .fill(.gray)
+                    .mask {
+                        Rectangle()
+                            .offset(x: -progressBarValue)
+                    }
+            }
+            .onChange(of: progress) { _, newValue in
+                progressBarValue = -geometry.size.width * newValue
+            }
+        }
+    }
+    
 }
 
 #Preview {
