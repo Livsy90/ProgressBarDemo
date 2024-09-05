60e64d0413728bbda89faed8dca355a8b6f8f91a
Livsy90 <artmir@live.ru>
2024-09-05 12:36:13 +0400

    Initial Commit

---
+++ ProgressBarDemo/ProgressBarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBarDemoApp.swift
+//  ProgressBarDemo
+//
+//  Created by Livsy on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
